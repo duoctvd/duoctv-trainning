abe2d4ef96e1ffedaf00e6be83b72b78fef9ec54
duoctv <duoctv@bit-vietnam.com>
2021-07-05 16:34:37 +0700

    static pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -6,43 +7,48 @@ export default function Home() {
       <Head>
         <title>duoctv trainning</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/css/bootstrap.min.css" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="">duoctv first training!</a >
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This is home page
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="" className="card">
+            <Link href="/article/1"><h3>Article 1 &rarr;</h3></Link>
+            <img src="/images/photo1.jpg" />
+            <p>This is article 1.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="" className="card">
+            <Link href="/article/2"><h3>Article 2 &rarr;</h3></Link>
+            <img src="/images/photo2.jpg" />
+            <p>This is article 2!</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href=""
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <Link href="/article/3"><h3>Article 31 &rarr;</h3></Link>
+            <img src="/images/photo3.jpg" />
+            <p>This is article 3.</p>
+          </a>
+
+          <a
+            href=""
+            className="card"
+          >
+            <Link href="/article/4"><h3>Article 4 &rarr;</h3></Link>
+            <img src="/images/photo4.jpg" />
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              This is article 4
             </p>
           </a>
         </div>
