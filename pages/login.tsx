45e22d47d398c0c2db2140f752e02cdc7758ec4f
duoctv <duoctv@bit-vietnam.com>
2021-07-23 18:36:33 +0700

    bug

--- pages/login.tsx
+++ pages/login.tsx
@@ -38,7 +38,6 @@ export default function Login() {
       // The signed-in user info.
     //  const user = result.user;
     router.push('/admin/top');
-    return <p>Logging...</p>
       // ...
     })
     .catch((error) => {
