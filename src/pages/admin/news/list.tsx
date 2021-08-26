6de73e31a7bc60b4348cd6a6ff2dc07b44c333a3
duoctv <duoctv@bit-vietnam.com>
2021-08-26 16:53:20 +0700

    apply swr for list page

--- src/pages/admin/news/list.tsx
+++ src/pages/admin/news/list.tsx
@@ -12,36 +12,30 @@ import { RetrieveNews } from "../../../firestore/news/retrieveNews";
 import { GetServerSideProps } from "next";
 import { InferGetServerSidePropsType } from "next";
 import "firebase/storage";
+import useSWR from 'swr';
 
-function List({
-  newsList,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  //  // Create a root reference
-  //  var storageRef = firebase.storage().ref();
-  //  let reader = new FileReader();
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-  // ;
-  //  // Create a reference to 'mountains.jpg'
-  //  var mountainsRef = storageRef.child('/photo1.jpg');
-  //  mountainsRef.put(reader.readAsText('/photo1.jpg')).then((snapshot) => {
-  //   console.log('Uploaded a blob or file!');
-  // });
-
+ function List() {
+  // let newsList: News[] = [];
+  const { data, error } = useSWR('/api/news/retrievenews', fetcher);
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  let newsList: News[] = data;
   return <NewsListTemplate newsList={newsList} />;
 }
-
 export default List;
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  var newsList: News[] = [];
-  // await the promise
-  newsList = await RetrieveNews();
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   var newsList: News[] = [];
+//   // await the promise
+//   newsList = await RetrieveNews();
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      newsList,
-    },
-  };
-};
+//   // By returning { props: { posts } }, the Blog component
+//   // will receive `posts` as a prop at build time
+//   return {
+//     props: {
+//       newsList,
+//     },
+//   };
+// };
